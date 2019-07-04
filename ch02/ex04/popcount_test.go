a839b700832bbaf7a0bd51a8e6bd668ba276d673
mactkg <mactkg@gmail.com>
2019-07-05 03:31:22 +0900

    ch02/ex04

---
+++ ch02/ex04/popcount_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/mactkg/golang_study/ch02/ex04/popcount"
+)
+
+func TestPopCounts(t *testing.T) {
+	single := popcount.PopCountSingle(10)
+	loopShift := popcount.PopCountLoopShift(10)
+	if single != loopShift {
+		t.Fatalf("failed: %v != %v", single, loopShift)
+	}
+}
+
+func BenchmarkPopCountSingle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountSingle(10)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountLoop(10)
+	}
+}
+
+func BenchmarkPopCountLoopShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountLoopShift(10)
+	}
+}
