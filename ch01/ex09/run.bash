c7920b4f88385636fc60aefe814ecd10ba00be7d
mactkg <mactkg@gmail.com>
2019-07-04 10:52:48 +0900

    ch01/ex09

---
+++ ch01/ex09/run.bash
@@ -0,0 +1,10 @@
+echo "https://httpbin.org/status/200"
+go run main.go https://httpbin.org/status/200
+
+echo ""
+echo "https://httpbin.org/status/404"
+go run main.go https://httpbin.org/status/404
+
+echo ""
+echo "https://httpbin.org/status/500"
+go run main.go https://httpbin.org/status/500
