c5dbf67ac3e0f15a29a66a1d6fab371d9ce9272e
mactkg <mactkg@gmail.com>
2019-09-29 22:02:02 +0900

    ch07/ex14: Import eval from ex13

---
+++ ch07/ex14/string.go
@@ -0,0 +1,32 @@
+package eval
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (v Var) String() string {
+	return string(v)
+}
+
+func (l literal) String() string {
+	return fmt.Sprintf("%f", l)
+}
+
+func (u unary) String() string {
+	return fmt.Sprintf("%s%s", string(u.op), u.x)
+}
+
+func (b binary) String() string {
+	return fmt.Sprintf("%s %s %s", b.x, string(b.op), b.y)
+}
+
+func (c call) String() string {
+	args := ""
+	for _, v := range c.args {
+		args += v.String() + ", "
+	}
+	args = strings.TrimRight(args, ", ")
+
+	return fmt.Sprintf("%s(%s)", c.fn, args)
+}
