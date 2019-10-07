d66f8f1e45b83ba899c1cc2d8013710f95b865f3
mactkg <mactkg@gmail.com>
2019-10-08 02:49:45 +0900

    New line code should be \r\n

--- ch08/ex02/main.go
+++ ch08/ex02/main.go
@@ -72,67 +72,67 @@ type FTPConnection struct {
 
 // 150
 func (c FTPConnection) replyOpeningDataConn() {
-	fmt.Fprintln(c, "150 File status okay; about to open data connection.")
+	fmt.Fprint(c, "150 File status okay; about to open data connection.\r\n")
 }
 
 // 200
 func (c FTPConnection) replyOkay() {
-	fmt.Fprintln(c, "200 Command okay.")
+	fmt.Fprint(c, "200 Command okay.\r\n")
 }
 
 // 220
 func (c FTPConnection) sendWelcome() {
-	fmt.Fprintln(c, "220 Service ready for new user.")
+	fmt.Fprint(c, "220 Service ready for new user.\r\n")
 }
 
 // 221
 func (c FTPConnection) replyClosingConn() {
-	fmt.Fprintln(c, "221 Service closing control connection.")
+	fmt.Fprint(c, "221 Service closing control connection.\r\n")
 }
 
 // 226
 func (c FTPConnection) replyClosingDataConn() {
-	fmt.Fprintln(c, "226 Closing data connection.")
+	fmt.Fprint(c, "226 Closing data connection.\r\n")
 }
 
 // 230
 func (c FTPConnection) replyLoggedIn() {
-	fmt.Fprintln(c, "230 User logged in, proceed.")
+	fmt.Fprint(c, "230 User logged in, proceed.\r\n")
 }
 
 // 425
 func (c FTPConnection) replyCantOpenDataConn() {
-	fmt.Fprintln(c, "425 Can't open data connection.")
+	fmt.Fprint(c, "425 Can't open data connection.\r\n")
 }
 
 // 426
 func (c FTPConnection) replyTransferAborted() {
-	fmt.Fprintln(c, "426 Connection closed; transfer aborted.")
+	fmt.Fprint(c, "426 Connection closed; transfer aborted.\r\n")
 }
 
 // 451
 func (c FTPConnection) replyLocalError() {
-	fmt.Fprintln(c, "451 Requested action aborted. Local error in processing.")
+	fmt.Fprint(c, "451 Requested action aborted. Local error in processing.\r\n")
 }
 
 // 501
 func (c FTPConnection) replyInvalidParamsError() {
-	fmt.Fprintln(c, "501 Syntax error in parameters or arguments.")
+	fmt.Fprint(c, "501 Syntax error in parameters or arguments.\r\n")
 }
 
 // 502
 func (c FTPConnection) replyInvalidActionError() {
-	fmt.Fprintln(c, "502 Command not implemented.")
+	fmt.Fprint(c, "502 Command not implemented.\r\n")
 }
 
 // 504
 func (c FTPConnection) replyParseParamsError() {
-	fmt.Fprintln(c, "504 Command not implemented for that parameter.")
+	fmt.Fprint(c, "504 Command not implemented for that parameter.\r\n")
 }
 
 // 530
 func (c FTPConnection) replyNotLoggedIn() {
-	fmt.Fprintf(c, "530 Not logged in.")
+	fmt.Fprint(c, "530 Not logged in.\r\n")
 }
 
 func (c FTPConnection) loginRequired() error {
@@ -171,7 +171,7 @@ func (c FTPConnection) ls(path string) error {
 		return fmt.Errorf("Local Error: (%v)", err)
 	}
 
-	_, err = dataConn.Write([]byte(strings.Join(filenames, "\t") + "\n"))
+	_, err = dataConn.Write([]byte(strings.Join(filenames, "\t") + "\r\n"))
 	if err != nil {
 		c.replyTransferAborted()
 	}
