86d8051f2423c000e65ba23619ca4c98587cf3ba
mactkg <mactkg@gmail.com>
2019-12-01 16:39:08 +0900

    ch12/ex05: marshal to json(not supported interface, func, complex)

--- ch12/ex05/sexpr_test.go
+++ ch12/ex05/sexpr_test.go
@@ -4,8 +4,8 @@
 package sexpr
 
 import (
+	"encoding/json"
 	"reflect"
-	"regexp"
 	"testing"
 )
 
@@ -48,7 +48,7 @@ func Test(t *testing.T) {
 	}
 
 	// Encode it
-	data, err := Marshal(strangelove)
+	data, err := MarshalJson(strangelove)
 	if err != nil {
 		t.Fatalf("Marshal failed: %v", err)
 	}
@@ -56,7 +56,7 @@ func Test(t *testing.T) {
 
 	// Decode it
 	var movie Movie
-	if err := Unmarshal(data, &movie); err != nil {
+	if err := json.Unmarshal(data, &movie); err != nil {
 		t.Fatalf("Unmarshal failed: %v", err)
 	}
 	t.Logf("Unmarshal() = %+v\n", movie)
@@ -64,98 +64,5 @@ func Test(t *testing.T) {
 	// Check equality.
 	if !reflect.DeepEqual(movie, strangelove) {
 		t.Fatal("not equal")
-	}
-
-	// Pretty-print it:
-	data, err = MarshalIndent(strangelove)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("MarshalIdent() = %s\n", data)
-}
-
-func TestPrettyPrint(t *testing.T) {
-	type Movie struct {
-		Title, Subtitle string
-		Year            int
-		Actor           map[string]string
-		Oscars          []string
-		Sequel          *string
-	}
-	strangelove := Movie{
-		Title:    "Dr. Strangelove",
-		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
-		Year:     1964,
-		Actor: map[string]string{
-			"Dr. Strangelove":            "Peter Sellers",
-			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
-			"Pres. Merkin Muffley":       "Peter Sellers",
-			"Gen. Buck Turgidson":        "George C. Scott",
-			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
-		},
-		Oscars: []string{
-			"Best Actor (Nomin.)",
-			"Best Adapted Screenplay (Nomin.)",
-			"Best Director (Nomin.)",
-			"Best Picture (Nomin.)",
-		},
-	}
-
-	// Pretty-print it:
-	data, err := Marshal(strangelove)
-	if err != nil {
-		t.Fatal(err)
-	}
-	role := `(Dr. Strangelove|Grp\. Capt\. Lionel Mandrake|Pres\. Merkin Muffley|Gen\. Buck Turgidson|Brig\. Gen\. Jack D\. Ripper|Maj\. T\.J\. "King" Kong)`
-	actor := `(Peter Sellers|George C\. Scott|Sterling Hayden|Slim Pickens)`
-	expect := `\(\(Title "Dr. Strangelove"\)
- \(Subtitle "How I Learned to Stop Worrying and Love the Bomb"\)
- \(Year 1964\)
- \(Actor \(\("` + role + `" "` + actor + `"\)
-         \("` + role + `" "` + actor + `"\)
-         \("` + role + `" "` + actor + `"\)
-         \("` + role + `" "` + actor + `"\)
-         \("` + role + `" "` + actor + `"\)\)\)
- \(Oscars \("Best Actor \(Nomin\.\)"
-          "Best Adapted Screenplay \(Nomin\.\)"
-          "Best Director \(Nomin\.\)"
-          "Best Picture \(Nomin\.\)"\)\)
- \(Sequel nil\)\)`
-	r := regexp.MustCompile(expect)
-	if !r.MatchString(string(data)) {
-		t.Fatalf("Wrong indent.\nGot:\n%s\n\nExpected:\n%s\n", data, expect)
-	}
-}
-
-func TestPPArray(t *testing.T) {
-	data, err := Marshal(struct {
-		ia []int
-	}{ia: []int{1, 2, 3, 4}})
-	if err != nil {
-		t.Fatal(err)
-	}
-	expect := `((ia (1
-      2
-      3
-      4)))`
-	if string(data) != expect {
-		t.Fatalf("Wrong indent.\nGot:\n%s\n\nExpected:\n%s\n", data, expect)
-	}
-}
-
-func TestPPStruct(t *testing.T) {
-	data, err := Marshal(struct {
-		ia    int
-		st    string
-		strct *struct{}
-	}{ia: 1, st: "string here", strct: nil})
-	if err != nil {
-		t.Fatal(err)
-	}
-	expect := `((ia 1)
- (st "string here")
- (strct nil))`
-	if string(data) != expect {
-		t.Fatalf("Wrong indent.\nGot:\n%s\n\nExpected:\n%s\n", data, expect)
 	}
 }
